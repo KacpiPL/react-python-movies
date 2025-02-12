74e485de16b967cb793e21be332755e5ba5d6e97
Kacper Gruca <kacpergruca@gmail.com>
2025-02-12 22:55:47 +0100

    Update MovieListItem

--- ui/src/MovieListItem.js
+++ ui/src/MovieListItem.js
@@ -11,6 +11,17 @@ export default function MovieListItem(props) {
                 <a onClick={props.onDelete}>Delete</a>
             </div>
             {props.movie.description}
+
+            {/* Wyświetlanie przypisanych aktorów */}
+            {Array.isArray(props.movie.actors) && props.movie.actors.length > 0 ? (
+                <ul>
+                    {props.movie.actors.map(actor => (
+                        <li key={actor.id}>{actor.name} {actor.surname}</li>
+                    ))}
+                </ul>
+            ) : (
+                <p>No actors assigned</p>
+            )}
         </div>
     );
 }
