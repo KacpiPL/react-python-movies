e58aba82ee2f4ce3d58b33e13944ad5877e7626f
Kacper <kacpergruca@gmail.com>
2025-02-13 13:51:31 +0100

    Add adding actor to film functionality

--- ui/src/App.js
+++ ui/src/App.js
@@ -3,21 +3,40 @@ import {useState, useEffect} from "react";
 import "milligram";
 import MovieForm from "./MovieForm";
 import MoviesList from "./MoviesList";
+import ActorsList from "./ActorsList";
+import AddActorForm from "./AddActorForm";
+import AssignActorForm from "./AssignActorForm";
 
 function App() {
     const [movies, setMovies] = useState([]);
+    const [actors, setActors] = useState([]);
     const [addingMovie, setAddingMovie] = useState(false);
+    const [assigningActor, setAssigningActor] = useState(false);
+    const [addingActor, setAddingActor] = useState(false);
+
+    // moved fetchMovies to a separate function
+    const fetchMovies = async () => {
+        const response = await fetch(`/movies`);
+        if (response.ok) {
+            const movies = await response.json();
+            setMovies(movies);
+        }
+    };
 
     useEffect(() => {
-      const fetchMovies = async () => {
-          const response = await fetch(`/movies`);
-          if (response.ok) {
-              const movies = await response.json();
-              setMovies(movies);
-          }
-      };
-      fetchMovies();
+        fetchMovies();
     }, []);
+
+    useEffect(() => {
+        const fetchActors = async () => {
+            const response = await fetch(`/actors`);
+            if (response.ok) {
+                const actors = await response.json();
+                setActors(actors);
+            }
+        };
+        fetchActors();
+      }, []);
 
     async function handleAddMovie(movie) {
         const response = await fetch('/movies', {
@@ -42,6 +61,31 @@ function App() {
         }
     }
 
+    async function handleAddActor(actor) {
+        const response = await fetch('/actors', {
+            method: 'POST',
+            body: JSON.stringify(actor),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (response.ok) {
+            const newActor = await response.json();
+            setActors([...actors, newActor]);
+        }
+    }
+
+    async function handleAssignActorToMovie(movieId, actorId) {
+        const response = await fetch(`/movies/${movieId}/actors`, {
+            method: 'POST',
+            body: JSON.stringify({ actor_id: actorId }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (response.ok) {
+            alert("Actor added to movie!");
+            setAssigningActor(false);
+            fetchMovies();
+        }
+    }
+
     return (
         <div className="container">
             <h1>My favourite movies to watch</h1>
@@ -49,12 +93,22 @@ function App() {
                 ? <p>No movies yet. Maybe add something?</p>
                 : <MoviesList movies={movies}
                               onDeleteMovie={(movie) => handleDeleteMovie(movie)}
+                              onAssignActor={() => setAssigningActor(true)}
+                              actors={actors}
                 />}
             {addingMovie
                 ? <MovieForm onMovieSubmit={handleAddMovie}
                              buttonLabel="Add a movie"
                 />
                 : <button onClick={() => setAddingMovie(true)}>Add a movie</button>}
+            <h2>Actors</h2>
+            <ActorsList actors={actors} />
+            {addingActor
+                ? <AddActorForm onActorSubmit={handleAddActor} />
+                : <button onClick={() => setAddingActor(true)}>Add an Actor</button>}
+            <h2>Actors in movies</h2>
+            {assigningActor && <AssignActorForm actors={actors} movies={movies} onAssign={handleAssignActorToMovie} />}
+            <button onClick={() => setAssigningActor(true)}>Assign an Actor to a Movie</button>
         </div>
     );
 }
